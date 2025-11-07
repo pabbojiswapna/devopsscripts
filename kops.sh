87f9eff49252ec5bd0b8324d328beada570da608
pabbojiswapna <pabboji.swapna@gmail.com>
2025-11-07 18:08:46 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -17,6 +17,7 @@ chmod 777 my-keypair.pub
 vi kops.sh
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 wget https://github.com/kubernetes/kops/releases/download/v1.32.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
@@ -24,7 +25,7 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket reyaz-kops-testbkt1433.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
 aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt1433.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://reyaz-kops-testbkt1433.k8s.local
-kops create cluster --name=reyaz.k8s.local --zones=ap-south-1a --control-plane-count=1 --control-plane-size=t3.medium --node-count=2 --node-size=t3.small --node-volume-size=20 --control-plane-volume-size=20 --ssh-public-key=my-keypair.pub --image=ami-02d26659fd82cf299 --networking=calico
+kops create cluster --name=reyaz.k8s.local --zones=ap-south-1a,ap-south-1b --control-plane-count=1 --control-plane-size=t3.medium --node-count=2 --node-size=t3.small --node-volume-size=20 --control-plane-volume-size=20 --ssh-public-key=my-keypair.pub --image=ami-02d26659fd82cf299 --networking=calico --topology=public
 kops update cluster --name reyaz.k8s.local --yes --admin
 
 
